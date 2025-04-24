ea9b2c912531afcaaa03afd29cb6ac89c4449bb9
kalvium <kalvium@kalvium.community>
2025-04-24 12:05:39 +0530

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+### RENDER DEPLOYED LINK:
+
+https://backend-ca1-72pr.onrender.com
+
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.post("/login",(req,res)=>{
 
 });
 
+app.get("/",(req,res)=>{
+    console.log("<h1>Server is running!</h1>")
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is running on: http://localhost:${PORT}`);
 });
