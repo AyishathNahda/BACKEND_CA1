cdd6d076ffcbb1302a2304eb622bb6215958ddca
kalvium <kalvium@kalvium.community>
2025-04-24 11:59:28 +0530

    login done

---
+++ server.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+const PORT=3000;
+
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body;
+    if(!email){
+        return res.status(400).json({error:"Email cannot be empty"});
+    }else if(!password){
+        return res.status(400).json({error:"Password cannot be empty"});
+    }else{
+        return res.status(200).json({message:"Login successfull!"});
+    }
+
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on: http://localhost:${PORT}`);
+});
+
